3e0e59686108cda14b3327b8db5cf0ab5d0f5bae
Udayasaran-kumar <ssaran3091998@gmail.com>
2025-05-05 16:28:18 +0530

    q1 added

---
+++ q1.js
@@ -0,0 +1,12 @@
+let arr=[3,4];
+for(let i=0;i<arr.length;i++){
+    if(arr[i]%2===0){
+        for(let j=arr[i];j>0;j--){
+            console.log("#".repeat(j));
+        }
+    }else {
+        for(let k=1;k<=arr[i];k++){
+            console.log("*".repeat(k));
+        }
+    }
+}
